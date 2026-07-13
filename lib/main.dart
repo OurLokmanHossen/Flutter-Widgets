b26e0584ddc3a450fcc7dd07115caa7092826dae
Lokman Hossen <lokmanhossen@MacBookPro.netis.cc>
2026-07-13 22:32:12 +0600

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/widgets/expanded.dart';
+import 'package:flutter_widgets/widgets/listView.dart';
 import 'package:flutter_widgets/widgets/textfeild.dart';
 
 void main() {
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: ExpandedWidgets()));
+    return MaterialApp(home: Scaffold(body: ListviewW()));
   }
 }
---
+++ lib/widgets/listView.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ListviewW extends StatelessWidget {
+  const ListviewW({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      physics: ClampingScrollPhysics(),
+      children: [
+        Image.network(
+          'https://i.pinimg.com/736x/f2/03/da/f203da942d758c6d0b281388269426e5.jpg',
+          fit: BoxFit.cover,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Image.network(
+            'https://i.pinimg.com/736x/f6/c6/19/f6c619af2edbfa44e7bab9930e0843c5.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
+  }
+}
